6b213a90295a030652ba3888f97d1dbe27667794
Ratimir <ra0953029364@gmail.com>
2019-12-07 16:26:43 +0200

    Добавьте файлы проекта.

---
+++ exercise_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите растояние");
+            int distance = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите простой");
+            int plain = int.Parse(Console.ReadLine());
+            if (distance >= 5)
+            {
+                int cost = (distance - 5 )*3 + 20 + plain;
+                Console.WriteLine($"Стоимость поездки {cost} грн.");
+            }
+            else
+            {
+                int cost = 20 + plain;
+                Console.WriteLine($"Стоимость поездки {cost} грн.");
+            }
+
+
+        }
+    }
+}
